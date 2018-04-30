f19ee3475ea8bbff8dfc08afdd8cf7ba0f3f6e96
Robin Vinck <robin.vinck@student.ehb.be>
2018-04-30 10:14:28 +0200

    test

--- ViewController.swift
+++ ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     var test:String = ""
+    var test2:String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
